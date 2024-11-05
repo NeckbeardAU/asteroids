5f7e45124e295ffe35cfc5d71b964d1b78718c1f
NeckbeardAU <85393711+NeckbeardAU@users.noreply.github.com>
2024-11-05 13:53:33 +1100

    initial commit.

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame #the main import
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen Width: {SCREEN_WIDTH}")
+    print(f"Screen Height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
